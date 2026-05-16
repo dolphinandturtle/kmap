0d64cce8c870ba40bcc6c7efb03e5ee23620d98d
Dolphin & Turtle <iamreallybadatphysics@gmail.com>
2026-05-16 15:30:22 +0200

    kadane encoding

--- include/kmap.h
+++ include/kmap.h
@@ -3,3 +3,8 @@
 
 void is_valid_kmap(size_t m, size_t n, bool kmap[m][n]);
 int count_max_rects(size_t m, size_t n);
+
+// Karnaugh map reduction rules are encoded into a kind
+// of maximum sub-matrix problem, that is solved with
+// the use of 'Kadane's Algorithm for a Torus'.
+void kadane_kmap(size_t m, size_t n, bool kmap[m][n], int kadane[m][n]);
--- src/kmap.c
+++ src/kmap.c
@@ -20,3 +20,16 @@ void is_valid_kmap(size_t m, size_t n, bool kmap[m][n]) {
 int count_max_rects(size_t m, size_t n) {
     return m * n / 2;
 }
+
+void kadane_kmap(size_t m, size_t n, bool kmap[m][n], int kadane[m][n]) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            // The kadane encoding is not unique.
+            // This encoding is NOT space-optimal,
+            // but IS time-optimal being that it
+            // is of the local kind.
+            kadane[i][j] = (kmap[i][j]) ? 1 : -(m + n);
+        }
+    }
+    return;
+}
